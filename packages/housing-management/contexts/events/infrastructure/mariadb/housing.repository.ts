b62e0889628bfe58674b3b390c49d6ee939c2d3d
Martín <martincativiela@gmail.com>
2025-12-12 10:28:29 +0100

    UPDATE build container changes

--- packages/housing-management/contexts/events/infrastructure/mariadb/housing.repository.ts
+++ packages/housing-management/contexts/events/infrastructure/mariadb/housing.repository.ts
@@ -161,7 +161,7 @@ export function housingRepositoryBuilder({
             }
         },
         async getAll(parameters: any, executedBy: AuthorizedUserProperties): Promise<Maybe<any>> {
-            console.debug("Testing get all");
+            console.debug("Testing get all 1");
             return [null, null];
 
             const { search, is_reserved, property_type, sede, page, perPage, asc, desc } =
